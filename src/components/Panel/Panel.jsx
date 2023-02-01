637c942bbe8134bd85fe11b97917f148afc405b7
Oleg Zasedatelew <ol.zasedatelew@yandex.ru>
2023-02-01 12:53:41 +0300

    first commit

---
+++ src/components/Panel/Panel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import FormFacts from "../Form/FormFacts";
+import FileUpload from "../Form/FileUpload";
+import List from "../Sections/List";
+import "./panel.css";
+
+const usid = require('usid');
+
+const Panel = () => {
+  return (
+    <React.Fragment>
+      <div className="facts-box">
+        <FormFacts key={usid.rand()} type={"facts"} />
+        <List key={usid.rand()} type={"facts"}></List>
+      </div>
+      <div className="gallery-box">
+        <FileUpload key={usid.rand()} type={"images"} />
+        <List key={usid.rand()} type={"images"}></List>
+      </div>
+    </React.Fragment>
+  );
+};
+export default Panel;
+/**  */
